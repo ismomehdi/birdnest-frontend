709173e3e13991691810b2cbca6d2e93f4eb76dc
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-01-09 20:52:10 +0200

    Add useEffect and setInterval

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  var [drones, setDrones] = useState([])
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      axios
+        .get('https://cors-anywhere.herokuapp.com/https://assignments.reaktor.com/birdnest/drones')
+        .then(response => {
+          let parser = new DOMParser()
+          let xmlDOM = parser.parseFromString(response.data, 'text/xml')      
+          setDrones(xmlDOM)
+          console.log(response.data)
+        })
+    }, 2000)
+  }, [])
+
+  return (console.log('Hello World'))
+}
 
 export default App
