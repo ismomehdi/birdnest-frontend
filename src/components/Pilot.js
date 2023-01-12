c24a5918e6fc08facbfe531d71ba3a6def7706d0
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-01-12 17:04:13 +0200

    Add convertedDistance

--- src/components/Pilot.js
+++ src/components/Pilot.js
@@ -1,10 +1,12 @@
 const Pilot = ( { pilot } ) => {
+    let convertedDistance = (pilot.distance / 1000).toFixed(2)
+    
     return (
         <tr>
             <td>{pilot.firstName} {pilot.lastName}</td>
             <td>{pilot.email}</td>
             <td>{pilot.phoneNumber}</td>
-            <td>{pilot.distance}</td>
+            <td>{convertedDistance} meters</td>
         </tr>
     )
 }
